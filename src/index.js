11b13d54f06481330a6a917f6e94902401063fea
Bentot23 <92287648+Bentot23@users.noreply.github.com>
2022-04-08 00:22:26 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('#create-task-form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    //console.log the e
+    console.log(e)
+    toDoList(e.target[0].value) // 0 is the id of description
+
+    form.reset()
+  })
 });
+
+function toDoList(toDo) {
+  let li = document.createElement('li') //to make a list for toDo
+  let btn = document.createElement('button') //to make a button beside the toDo
+  btn.addEventListener('click', deleteTask)
+  btn.textContent = 'X'
+  let ul = document.querySelector('#tasks')
+  ul.appendChild(li)
+  li.innerText = `${toDo} `
+  li.appendChild(btn)
+}
+
+function deleteTask(e) {
+  e.target.parentNode.remove()
+}
